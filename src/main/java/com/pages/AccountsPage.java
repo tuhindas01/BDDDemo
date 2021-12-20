c63676846bc39d0f463ab28af23ffc419c598a7e
tuhindas01 <dastuhin291@gmail.com>
2021-12-20 19:08:50 +0530

    Modified TestUtill Class

--- src/main/java/com/pages/AccountsPage.java
+++ src/main/java/com/pages/AccountsPage.java
@@ -7,9 +7,12 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import com.utill.TestUtill;
+
 public class AccountsPage {
 	
 	private WebDriver driver;
+	private TestUtill testUtill = new TestUtill();
 	
 	private By accountSections = By.cssSelector("div#center_column span");
 	
@@ -18,17 +21,17 @@ public class AccountsPage {
 	}
 	
 	public String getAccountsPageTitle() {
-		return driver.getTitle();
+		return testUtill.doGetTitle(driver);
 	}
 	
 	public int getAccountSectionCount() {
-		List<WebElement> accountsSectionCnt = driver.findElements(accountSections);
+		List<WebElement> accountsSectionCnt = testUtill.doGetListOfElements(driver, accountSections);
 		return accountsSectionCnt.size()-1;
 	}
 	
 	public List<String> getAccountSectionList() {
 		List<String> accountList = new ArrayList<>();
-		List<WebElement> accountsSectionList = driver.findElements(accountSections);
+		List<WebElement> accountsSectionList = testUtill.doGetListOfElements(driver, accountSections);
 		
 		for(WebElement e : accountsSectionList) {
 			String listElementName = e.getText();
